7ffdd7e6dd30bbe0b811bde351e32e20850e488f
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-04-29 03:11:24 +0530

    changes

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
-# Update this file to run your own code
+
+
+# Install secp256k1 module
+npm install secp256k1
+
+# Check if installation was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to install secp256k1 module"
+  exit 1
+fi
+
+# Run index.js
 node index.js
